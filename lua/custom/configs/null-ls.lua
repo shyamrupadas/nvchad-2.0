6e411bba469cf517c36c5e5c79494584a99667a9
Sergey Maksimov <s.mksimov@gmail.com>
2024-12-05 23:35:06 +0300

    init

---
+++ lua/custom/configs/null-ls.lua
@@ -0,0 +1,37 @@
+local null_ls = require "null-ls"
+
+local b = null_ls.builtins
+
+local sources = {
+
+  -- webdev stuff
+  b.formatting.prettier, -- choosed deno for ts/js files cuz its very fast!
+  b.diagnostics.eslint_d,
+
+  -- Lua
+  b.formatting.stylua,
+
+  -- cpp
+  b.formatting.clang_format,
+}
+
+local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
+
+null_ls.setup {
+  debug = true,
+  sources = sources,
+
+  -- you can reuse a shared lspconfig on_attach callback here
+  on_attach = function(client, bufnr)
+    if client.supports_method "textDocument/formatting" then
+      vim.api.nvim_clear_autocmds { group = augroup, buffer = bufnr }
+      vim.api.nvim_create_autocmd("BufWritePre", {
+        group = augroup,
+        buffer = bufnr,
+        callback = function()
+          vim.lsp.buf.format { async = false }
+        end,
+      })
+    end
+  end,
+}
